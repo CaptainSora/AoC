3a94bcbe263ebe3b721f8d359ce1f3bb1aa95f1b
Joseph Wang <joseph4x4@gmail.com>
2020-12-06 01:24:20 -0500

    2019 Day 5 and 6

---
+++ 2019/day05.py
@@ -0,0 +1,115 @@
+def day05a():
+    with open("2019/day05_input.txt", 'r') as f:
+        opcode = [int(num) for num in f.read().strip().split(',')]
+
+    input = 1
+    pos = 0
+    while(str(opcode[pos])[-2:] != "99"):
+        a, b, c = opcode[pos+1:pos+4]
+        instr = f'{opcode[pos]:05}'
+        if instr[-2:] == '01':
+            a, b, c = opcode[pos+1:pos+4]
+            if instr[2] == '0':
+                a = opcode[a]
+            if instr[1] == '0':
+                b = opcode[b]
+            opcode[c] = a + b
+            pos += 4
+        elif instr[-2:] == '02':
+            a, b, c = opcode[pos+1:pos+4]
+            if instr[2] == '0':
+                a = opcode[a]
+            if instr[1] == '0':
+                b = opcode[b]
+            opcode[c] = a * b
+            pos += 4
+        elif instr[-2:] == '03':
+            a = opcode[pos+1]
+            opcode[a] = input
+            pos += 2
+        elif instr[-2:] == '04':
+            a = opcode[pos+1]
+            if instr[2] == '0':
+                a = opcode[a]
+            print(a)
+            pos += 2
+        else:
+            print("Houston we have a problem")
+            return -1
+
+def day05b():
+    with open("2019/day05_input.txt", 'r') as f:
+        opcode = [int(num) for num in f.read().strip().split(',')]
+
+    input = 5
+    pos = 0
+    while(str(opcode[pos])[-2:] != "99"):
+        a, b, c = opcode[pos+1:pos+4]
+        instr = f'{opcode[pos]:05}'
+        if instr[-2:] == '01':
+            a, b, c = opcode[pos+1:pos+4]
+            if instr[2] == '0':
+                a = opcode[a]
+            if instr[1] == '0':
+                b = opcode[b]
+            opcode[c] = a + b
+            pos += 4
+        elif instr[-2:] == '02':
+            a, b, c = opcode[pos+1:pos+4]
+            if instr[2] == '0':
+                a = opcode[a]
+            if instr[1] == '0':
+                b = opcode[b]
+            opcode[c] = a * b
+            pos += 4
+        elif instr[-2:] == '03':
+            a = opcode[pos+1]
+            opcode[a] = input
+            pos += 2
+        elif instr[-2:] == '04':
+            a = opcode[pos+1]
+            if instr[2] == '0':
+                a = opcode[a]
+            print(a)
+            pos += 2
+        elif instr[-2:] == '05':
+            a, b = opcode[pos+1:pos+3]
+            if instr[2] == '0':
+                a = opcode[a]
+            if instr[1] == '0':
+                b = opcode[b]
+            if a != 0:
+                pos = b
+            else:
+                pos += 3
+        elif instr[-2:] == '06':
+            a, b = opcode[pos+1:pos+3]
+            if instr[2] == '0':
+                a = opcode[a]
+            if instr[1] == '0':
+                b = opcode[b]
+            if a == 0:
+                pos = b
+            else:
+                pos += 3
+        elif instr[-2:] == '07':
+            a, b, c = opcode[pos+1:pos+4]
+            if instr[2] == '0':
+                a = opcode[a]
+            if instr[1] == '0':
+                b = opcode[b]
+            opcode[c] = int(a < b)
+            pos += 4
+        elif instr[-2:] == '08':
+            a, b, c = opcode[pos+1:pos+4]
+            if instr[2] == '0':
+                a = opcode[a]
+            if instr[1] == '0':
+                b = opcode[b]
+            opcode[c] = int(a == b)
+            pos += 4
+        else:
+            print("Houston we have a problem")
+            return -1
+
+day05b()
---
+++ 2019/day06.py
@@ -0,0 +1,42 @@
+def day06a():
+    with open("2019/day06_input.txt", 'r') as f:
+        orbits = [
+            pair.strip().split(')') 
+            for pair in f.read().strip().split('\n')
+        ]
+    
+    orbit_dict = {'COM': 0}
+    pos = 0
+    while orbits:
+        while orbits[pos][0] not in orbit_dict:
+            pos += 1
+        orbit_dict[orbits[pos][1]] = orbit_dict[orbits[pos][0]] + 1
+        orbits.pop(pos)
+        pos = 0
+    
+    return sum(orbit_dict.values())
+
+def day06b():
+    with open("2019/day06_input.txt", 'r') as f:
+        orbits = [
+            pair.strip().split(')') 
+            for pair in f.read().strip().split('\n')
+        ]
+    
+    santa_path = ['SAN']
+    while santa_path[-1] != 'COM':
+        for pair in orbits:
+            if pair[1] == santa_path[-1]:
+                santa_path.append(pair[0])
+                break
+    down_transfers = -1
+    cur_pos = 'YOU'
+    while cur_pos not in santa_path:
+        for pair in orbits:
+            if pair[1] == cur_pos:
+                down_transfers += 1
+                cur_pos = pair[0]
+                break
+    return down_transfers + santa_path.index(cur_pos) - 1
+
+print(day06b())
