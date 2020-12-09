5a3f2942d6e5e58cca47f10673851f7c08c0b0eb
Joseph Wang <joseph4x4@gmail.com>
2020-12-09 00:20:46 -0500

    AoC 2020 Day 9 - 64th global (Part 1) and 39th global (Part 2)

--- 2020/day09.py
+++ 2020/day09.py
@@ -1,9 +1,39 @@
 def day09a():
 	with open("2020/day09_input.txt", "r") as f:
-		pass
+		xmas = [int(num) for num in f.read().strip().split('\n')]
+	
+	for i in range(25, len(xmas)):
+		target = xmas[i]
+		found = False
+		for j in xmas[i-25:i]:
+			if target - j in xmas[i-25:i]:
+				found = True
+				break
+		if not found:
+			return xmas[i]
 
 def day09b():
 	with open("2020/day09_input.txt", "r") as f:
-		pass
+		xmas = [int(num) for num in f.read().strip().split('\n')]
+	
+	invalid = 0
+	for i in range(25, len(xmas)):
+		target = xmas[i]
+		found = False
+		for j in xmas[i-25:i]:
+			if target - j in xmas[i-25:i]:
+				found = True
+				break
+		if not found:
+			invalid = xmas[i]
+			break
+	
+	length = 2
+	while True:
+		for i in range(len(xmas) - length + 1):
+			contig = xmas[i:i+length]
+			if sum(contig) == invalid:
+				return min(contig) + max(contig)
+		length += 1
 
-print(day09a())
+print(day09b())
