13f3fe6dc326f116bd366a863835bbb2af268f4f
Joseph Wang <joseph4x4@gmail.com>
2020-12-15 00:24:53 -0500

    AOC 202 Day 15 - 29th global (Part 1)

--- 2020/day15.py
+++ 2020/day15.py
@@ -1,9 +1,44 @@
 def day15a():
-	with open("2020/day15_input.txt", "r") as f:
-		pass
+	instr = [int(num) for num in "8,13,1,0,18,9".split(",")]
+	
+	speak = instr[:]
+	while len(speak) < 2020:
+		last = speak[-1]
+		if last not in speak[:-1]:
+			speak.append(0)
+		else:
+			prev = 1
+			while speak[-1 * prev - 1] != last:
+				prev += 1
+			speak.append(prev)
 
+		
+	return speak[-1]
+	
+
+# WARNING: CAN TAKE A FEW SECONDS, uncomment print lines for verification
 def day15b():
-	with open("2020/day15_input.txt", "r") as f:
-		pass
+	instr = [int(num) for num in "8,13,1,0,18,9".split(",")]
+	
+	speak = {}
+	counter = 1
+	for i in instr[:-1]:
+		speak[str(i)] = counter
+		counter += 1
+	last = str(instr[-1])
+	while counter < 30000000:
+		# if counter % 1000000 == 0:
+		# 	print(counter)
+		if last not in speak.keys():
+			speak[last] = counter
+			last = "0"
+			counter += 1
+		else:
+			diff = counter - speak[last]
+			speak[last] = counter
+			last = str(diff)
+			counter += 1
 
-print(day15a())
+	return last
+
+print(day15b())
