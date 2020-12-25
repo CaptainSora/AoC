408cd04257101545a87c56f4c2661754db87c334
Joseph Wang <joseph4x4@gmail.com>
2020-12-25 00:12:29 -0500

    AoC 2020 Complete!

--- 2020/day25.py
+++ 2020/day25.py
@@ -1,9 +1,26 @@
 def day25a():
 	with open("2020/day25_input.txt", "r") as f:
-		pass
+		cpub, dpub = [int(num) for num in f.read().strip().split('\n')]
+	
+	def get_loop_size(pub):
+		value = 1
+		loops = 0
+		while value != pub:
+			value = (value * 7) % 20201227
+			loops += 1
+		return loops
+	
+	def transform(pub, loop):
+		value = 1
+		for _ in range(loop):
+			value = (value * pub) % 20201227
+		return value
+	
+	cloop = get_loop_size(cpub)
+	return transform(dpub, cloop)
+
 
 def day25b():
-	with open("2020/day25_input.txt", "r") as f:
-		pass
+	return True
 
 print(day25a())
