71e870c7975b5675cfb3a18d2b6f12f29c64fa49
Joseph Wang <joseph4x4@gmail.com>
2022-12-06 00:07:53 -0500

    Day 6

--- 2022/day06.py
+++ 2022/day06.py
@@ -1,10 +1,16 @@
 def day06a():
 	with open("2022/day06_input.txt", "r") as f:
-		pass
+		datastream = f.read()
+	for i in range(4, len(datastream)):
+		if len(set(datastream[i-3:i+1])) == 4:
+			return i+1
 
 def day06b():
 	with open("2022/day06_input.txt", "r") as f:
-		pass
+		datastream = f.read()
+	for i in range(14, len(datastream)):
+		if len(set(datastream[i-13:i+1])) == 14:
+			return i+1
 
 
 print(day06a())
