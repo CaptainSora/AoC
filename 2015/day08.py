f4071798043d32b86182656c1504b802b031020d
Joseph Wang <joseph4x4@gmail.com>
2022-12-06 23:18:29 -0500

    Day 8

--- 2015/day08.py
+++ 2015/day08.py
@@ -1,10 +1,27 @@
 def day08a():
 	with open("2015/day08_input.txt", "r") as f:
-		pass
+		santa = f.read().strip().split("\n")
+	chrdiff = 0
+	for line in santa:
+		chrdiff += len(line)
+		escape = 0
+		for chr in line[1:-1]:
+			if escape and chr == "x":
+				escape = 2
+			elif escape:
+				escape -= 1
+				if not escape:
+					chrdiff -= 1
+			elif chr == "\\":
+				escape = 1
+			else:
+				chrdiff -= 1
+	return chrdiff
 
 def day08b():
 	with open("2015/day08_input.txt", "r") as f:
-		pass
+		santa = f.read().strip().split("\n")
+	return sum([2 + line.count("\\") + line.count('"') for line in santa])
 
 
 print(day08a())
