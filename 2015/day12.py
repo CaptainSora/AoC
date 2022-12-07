35cee4056f9ae848f8512c12be33cf0c1fe6d4b5
Joseph Wang <joseph4x4@gmail.com>
2022-12-07 01:12:20 -0500

    Day 12

--- 2015/day12.py
+++ 2015/day12.py
@@ -1,10 +1,35 @@
+from collections import deque
+from json import loads
+
+
 def day12a():
 	with open("2015/day12_input.txt", "r") as f:
-		pass
+		q = deque(loads(f.read()))
+	total = 0
+	while q:
+		item = q.popleft()
+		if type(item) == list:
+			q.extend(item)
+		elif type(item) == dict:
+			q.extend(item.values())
+		elif type(item) == int:
+			total += item
+	return total
 
 def day12b():
 	with open("2015/day12_input.txt", "r") as f:
-		pass
+		q = deque(loads(f.read()))
+	total = 0
+	while q:
+		item = q.popleft()
+		if type(item) == list:
+			q.extend(item)
+		elif type(item) == dict:
+			if "red" not in item and "red" not in item.values():
+				q.extend(item.values())
+		elif type(item) == int:
+			total += item
+	return total
 
 
 print(day12a())
