595f4090f5ef2261cc8b3cd151422c02c587a5d9
Joseph Wang <joseph4x4@gmail.com>
2022-12-06 23:23:46 -0500

    Day 10

--- 2015/day10.py
+++ 2015/day10.py
@@ -1,10 +1,34 @@
 def day10a():
-	with open("2015/day10_input.txt", "r") as f:
-		pass
+	input = "1113222113"
+	output = ""
+	for _ in range(40):
+		prev = input[0]
+		for d in input[1:]:
+			if d != prev[-1]:
+				output += str(len(prev)) + prev[0]
+				prev = d
+			else:
+				prev += d
+		if prev:
+			output += str(len(prev)) + prev[0]
+		input, output = output, ""
+	return len(input)
 
 def day10b():
-	with open("2015/day10_input.txt", "r") as f:
-		pass
+	input = "1113222113"
+	output = ""
+	for _ in range(50):
+		prev = input[0]
+		for d in input[1:]:
+			if d != prev[-1]:
+				output += str(len(prev)) + prev[0]
+				prev = d
+			else:
+				prev += d
+		if prev:
+			output += str(len(prev)) + prev[0]
+		input, output = output, ""
+	return len(input)
 
 
 print(day10a())
