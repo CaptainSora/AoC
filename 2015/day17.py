e03c767ed5fc5f5dc82028590c56329350f71d67
Joseph Wang <joseph4x4@gmail.com>
2022-12-07 22:20:52 -0500

    Day 17

--- 2015/day17.py
+++ 2015/day17.py
@@ -1,10 +1,22 @@
+from itertools import combinations
+
+
 def day17a():
 	with open("2015/day17_input.txt", "r") as f:
-		pass
+		containers = [int(c) for c in f.read().strip().split("\n")]
+	return sum([
+		sum(comb) == 150
+		for i in range(1, len(containers) + 1)
+		for comb in combinations(containers, i)
+	])
 
 def day17b():
 	with open("2015/day17_input.txt", "r") as f:
-		pass
+		containers = [int(c) for c in f.read().strip().split("\n")]
+	for i in range(1, len(containers) + 1):
+		num = sum([sum(comb) == 150 for comb in combinations(containers, i)])
+		if num > 0:
+			return num
 
 
 print(day17a())
