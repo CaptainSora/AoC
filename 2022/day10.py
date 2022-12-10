cefaa48dac203e2f627a566997358dd07dae8723
Joseph Wang <joseph4x4@gmail.com>
2022-12-10 01:06:16 -0500

    Day 10

--- 2022/day10.py
+++ 2022/day10.py
@@ -1,10 +1,46 @@
 def day10a():
 	with open("2022/day10_input.txt", "r") as f:
-		pass
+		cmd = f.read().strip().split("\n")
+	signal = 0
+	x = 1
+	cycles = [20, 60, 100, 140, 180, 220]
+	cycle = 1
+
+	def tick():
+		nonlocal cycle, signal, x
+		cycle += 1
+		if cycle in cycles:
+			signal += x * cycle
+	
+	for instr in cmd:
+		instr = instr.split()
+		if instr[0] == "addx":
+			tick()
+			x += int(instr[1])
+		tick()
+	return signal
 
 def day10b():
 	with open("2022/day10_input.txt", "r") as f:
-		pass
+		cmd = f.read().strip().split("\n")
+	x = 1
+	cycle = 0
+	screen = [["." for _ in range(40)] for _ in range(6)]
+
+	def tick():
+		nonlocal cycle, screen, x
+		cycle += 1
+		r, c = divmod(cycle, 40)
+		if abs(c - x) <= 1:
+			screen[r][c] = "#"
+	
+	for instr in cmd:
+		instr = instr.split()
+		if instr[0] == "addx":
+			tick()
+			x += int(instr[1])
+		tick()
+	return "\n".join(["".join(row) for row in screen])
 
 
 print(day10a())
