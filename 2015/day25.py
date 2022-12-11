62078a7b2a3f0ceab95192bed22da7c2c4c532ae
Joseph Wang <joseph4x4@gmail.com>
2022-12-10 23:50:18 -0500

    Day 25

--- 2015/day25.py
+++ 2015/day25.py
@@ -1,10 +1,26 @@
+def poly(n, t):
+    """
+    Returns term t of the n-gonal numbers.
+    Works for all n >= 1 and t >= 1.
+    """
+    if n < 1 or t < 1:
+        return 0
+    elif n == 1:
+        return 1
+    return int(t * ((n-2) * t - (n-4)) / 2)
+
+
 def day25a():
 	with open("2015/day25_input.txt", "r") as f:
-		pass
+		r, c = [int(num[:-1]) for num in f.read().split()[-3::2]]
+	codenum = poly(3, c) + int((c + c + r - 2) * (r - 1) / 2)
+	code = 20151125
+	for _ in range(1, codenum):
+		code = (code * 252533) % 33554393
+	return code
 
 def day25b():
-	with open("2015/day25_input.txt", "r") as f:
-		pass
+	return True
 
 
 print(day25a())
