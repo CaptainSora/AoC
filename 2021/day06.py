ff1f98c02f513bb704b5e5ddec7784709f67bce2
Joseph Wang <joseph4x4@gmail.com>
2022-12-12 16:19:24 -0500

    Day 6

--- 2021/day06.py
+++ 2021/day06.py
@@ -1,10 +1,23 @@
+from collections import deque
+
+
 def day06a():
 	with open("2021/day06_input.txt", "r") as f:
-		pass
+		lantern = [int(num) for num in f.read().strip().split(",")]
+	ages = deque([lantern.count(i) for i in range(9)])
+	for _ in range(80):
+		ages.rotate(-1)
+		ages[6] += ages[-1]
+	return(sum(ages))
 
 def day06b():
 	with open("2021/day06_input.txt", "r") as f:
-		pass
+		lantern = [int(num) for num in f.read().strip().split(",")]
+	ages = deque([lantern.count(i) for i in range(9)])
+	for _ in range(256):
+		ages.rotate(-1)
+		ages[6] += ages[-1]
+	return(sum(ages))
 
 
 print(day06a())
