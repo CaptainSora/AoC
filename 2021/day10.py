c4bc09da13ab7cb1c59df4b785f47a6763360f8a
Joseph Wang <joseph4x4@gmail.com>
2022-12-13 23:26:39 -0500

    Day 10

--- 2021/day10.py
+++ 2021/day10.py
@@ -1,10 +1,41 @@
 def day10a():
 	with open("2021/day10_input.txt", "r") as f:
-		pass
+		navsys = f.read().strip().split("\n")
+	errorscore = 0
+	for line in navsys:
+		chunk = []
+		for chr in line:
+			if chr in "([{<":
+				chunk.append(chr)
+			else:
+				idx = ")]}>".index(chr)
+				if chunk[-1] != "([{<"[idx]:
+					errorscore += [3, 57, 1197, 25137][idx]
+					break
+				chunk.pop()
+	return errorscore
 
 def day10b():
 	with open("2021/day10_input.txt", "r") as f:
-		pass
+		navsys = f.read().strip().split("\n")
+	compscore = []
+	for line in navsys:
+		chunk = []
+		for chr in line:
+			if chr in "([{<":
+				chunk.append(chr)
+			else:
+				idx = ")]}>".index(chr)
+				if chunk[-1] != "([{<"[idx]:
+					break
+				chunk.pop()
+		else:
+			score = 0
+			while chunk:
+				score *= 5
+				score += "([{<".index(chunk.pop()) + 1
+			compscore.append(score)
+	return sorted(compscore)[len(compscore)//2]
 
 
 print(day10a())
