45fcdda6ce8f9ac4206a45807d40ecf3fdad34a8
Joseph Wang <joseph4x4@gmail.com>
2023-12-01 01:20:56 -0500

    2021 Day 13

--- 2021/day13.py
+++ 2021/day13.py
@@ -1,10 +1,41 @@
 def day13a():
 	with open("2021/day13_input.txt", "r") as f:
-		pass
+		dots, folds = f.read().strip().split("\n\n")
+	dots = [[int(n) for n in line.split(",")] for line in dots.split("\n")]
+	folds = [
+		["xy".index(line[11]), int(line[13:])]
+		for line in folds.split("\n")
+	]
+	dir, value = folds[0]
+	for i in range(len(dots)):
+		if dots[i][dir] > value:
+			dots[i][dir] = 2 * value - dots[i][dir]
+	return len(set([tuple(d) for d in dots]))
+			
+
 
 def day13b():
 	with open("2021/day13_input.txt", "r") as f:
-		pass
+		dots, folds = f.read().strip().split("\n\n")
+	dots = [[int(n) for n in line.split(",")] for line in dots.split("\n")]
+	folds = [
+		["xy".index(line[11]), int(line[13:])]
+		for line in folds.split("\n")
+	]
+	bounds = [None, None]
+	for fold in folds:
+		dir, value = fold
+		bounds[dir] = value
+		for i in range(len(dots)):
+			if dots[i][dir] > value:
+				dots[i][dir] = 2 * value - dots[i][dir]
+	dots = set([tuple(d) for d in dots])
+	# Draw image
+	for y in range(bounds[1]):
+		print("".join([
+			"#" if (x, y) in dots else "."
+			for x in range(bounds[0])
+		]))
 
 
 print(day13a())
