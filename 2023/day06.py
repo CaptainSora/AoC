4d5904e359d6df9adab316b724b34bf8d267c07a
Joseph Wang <joseph4x4@gmail.com>
2023-12-06 00:09:17 -0500

    2023 Day 6

--- 2023/day06.py
+++ 2023/day06.py
@@ -1,10 +1,40 @@
+from math import prod
+
+
 def day06a():
 	with open("2023/day06_input.txt", "r") as f:
-		pass
+		time, distance = [
+			[int(num) for num in line.split()[1:] if num]
+			for line in f.read().strip().split("\n")
+		]
+	win_options = [0 for _ in distance]
+	for hold_dur in range(1, time[-1]):
+		for idx, t in enumerate(time):
+			dist = hold_dur * (t - hold_dur)
+			if dist > distance[idx]:
+				win_options[idx] += 1
+	return prod(win_options)
+
 
 def day06b():
 	with open("2023/day06_input.txt", "r") as f:
-		pass
+		time, distance = [
+			int(line.split(":")[-1])
+			for line in f.read().strip().replace(" ", "").split("\n")
+		]
+	print(time, distance)
+	winners = []
+	for hold_dur in range(1, time):
+		dist = hold_dur * (time - hold_dur)
+		if dist > distance:
+			winners.append(hold_dur)
+			break
+	for hold_dur in range(time, 1, -1):
+		dist = hold_dur * (time - hold_dur)
+		if dist > distance:
+			winners.append(hold_dur)
+			break
+	return winners[1] - winners[0] + 1
 
 
 print(day06a())
